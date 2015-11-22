6b2b5e5c69645c634778f6e36189f6459175cb82
Iain Peet <iain@ipeet.org>
2015-11-22 12:28:35 -0800

    PyFile parses file and resolves imports

--- graph/file.py
+++ graph/file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import ast
+import logging
 import graph.edge as edge
 from graph.node import Node
 import imp
@@ -35,39 +36,58 @@ class PyFile(Node):
     def __init__(self, path, finder=imp.find_module):
         super(PyFile, self).__init__()
         self.path = path
-        self._load()
+        self.finder = finder
+        self.imports = set()
+        self._load_imports()
         
-    def _load(self):
+    def _load_imports(self):
         with open(self.path) as f:
             tree = ast.parse(f.read())
             
-        imports = []
+        parsed = [] #[(name, maybe_not_module), ...]
         for n in ast.walk(tree):
             if isinstance(n, ast.Import):
                 for name in n.names:
-                    imports.append(name.name)
-         
-        for name in imports:
-            pass
+                    parsed.append((name.name, False))
+            if isinstance(n, ast.ImportFrom):
+                parsed.append((n.module, False))
+                for name in n.names:
+                    parsed.append(
+                        ('{}.{}'.format(n.module, name.name), True))
+        
+        new_imports = set() 
+        for name, maybe_not_module in parsed:
+            try:
+                parent, paths = resolve_import(name, self.finder)
+                new_imports.update(set(os.path.abspath(p) for p in paths))
+            except ImportError as e:
+                if not maybe_not_module:
+                    # ImportError is not interesting if this is a name in 
+                    # "from mod import names", as it's probably a non-module
+                    # name.
+                    logging.info('Failed to resolve {}:{}: {}'.format(
+                        self.path, name, e))
+        self.imports = new_imports            
         
 import tempfile
 import unittest
 import shutil
 
-class ResolveImportTest(unittest.TestCase):
-    def make_finder(self, modules):
-        def res(name, paths):
-            paths = tuple(paths) if paths else None
-            if (name, paths) in modules:
-                path, typ = modules[(name, paths)]
-                return (None, path, (None, None, typ))
-            else:
-                raise ImportError('Failed to find {} in {}'.format(
-                    name, paths))
-        return res
-        
+
+def make_finder(modules):
+    def res(name, paths):
+        paths = tuple(paths) if paths else None
+        if (name, paths) in modules:
+            path, typ = modules[(name, paths)]
+            return (None, path, (None, None, typ))
+        else:
+            raise ImportError('Failed to find {} in {}'.format(
+                name, paths))
+    return res
+
+class ResolveImportTest(unittest.TestCase):        
     def test_resolve_top(self):
-        mock_finder = self.make_finder({
+        mock_finder = make_finder({
             ('mod', None): ('mod.py', imp.PY_SOURCE)
         })
         parent, paths = resolve_import('mod', mock_finder)
@@ -75,7 +95,7 @@ class ResolveImportTest(unittest.TestCase):
         self.assertEqual(paths, ['mod.py'])
         
     def test_resolve_pkg(self):
-        mock_finder = self.make_finder({
+        mock_finder = make_finder({
             ('pkg', None): ('pkg/', imp.PKG_DIRECTORY)
         })
         parent, paths = resolve_import('pkg', mock_finder)
@@ -83,7 +103,7 @@ class ResolveImportTest(unittest.TestCase):
         self.assertEqual(paths, ['pkg/__init__.py'])
         
     def test_resolve_in_pkg(self):
-        mock_finder = self.make_finder({
+        mock_finder = make_finder({
             ('mod', ('pkg/',)): ('pkg/mod.py', imp.PY_SOURCE),
             ('pkg', None): ('pkg/', imp.PKG_DIRECTORY)
         })
@@ -95,6 +115,11 @@ class PyFileTest(unittest.TestCase):
     def setUp(self):
         self.dir = tempfile.mkdtemp()
         self.src = os.path.join(self.dir, 'src.py')
+        self.modules = {
+            ('mod', ('/root/pkg/',)): ('/root/pkg/mod.py', imp.PY_SOURCE),
+            ('pkg', ('/root/',)): ('/root/pkg/', imp.PKG_DIRECTORY),
+            ('root', None): ('/root/', imp.PKG_DIRECTORY)
+        }
         
     def tearDown(self):
         shutil.rmtree(self.dir)
@@ -105,11 +130,41 @@ class PyFileTest(unittest.TestCase):
         
     def test_load_import(self):
         with open(self.src, 'w') as f:
-            f.write("import graph.file")
-        p = PyFile(self.src)
-        self.assertEqual(p.outgoing, 
-            {os.path.abspath('graph/file.py')})
-
+            f.write('import root.pkg.mod')
+        mock_finder = make_finder(self.modules)
+        p = PyFile(self.src, mock_finder)
+        self.assertEqual(p.imports, 
+            {'/root/__init__.py', '/root/pkg/__init__.py', '/root/pkg/mod.py'})
+            
+    def test_load_multi_import_as(self):
+        with open(self.src, 'w') as f:
+            f.write('import root.foo as f, root.pkg.mod as m')
+        self.modules.update({
+            ('foo', ('/root/',)): ('/root/foo.py', imp.PY_SOURCE)
+        })
+        p = PyFile(self.src, make_finder(self.modules))
+        self.assertEqual(p.imports,  {
+            '/root/__init__.py', 
+            '/root/foo.py',
+            '/root/pkg/__init__.py', 
+            '/root/pkg/mod.py'
+        })
+        
+    def test_load_from_import_nonmod(self):
+        with open(self.src, 'w') as f:
+            f.write('from root.pkg import Classy')
+        p = PyFile(self.src, make_finder(self.modules))
+        self.assertEqual(p.imports, 
+            {'/root/__init__.py', '/root/pkg/__init__.py'})
+            
+    def test_load_from_import_mod(self):
+        with open(self.src, 'w') as f:
+            f.write('from root.pkg import mod')
+        p = PyFile(self.src, make_finder(self.modules))
+        self.assertEqual(p.imports, 
+            {'/root/__init__.py', '/root/pkg/__init__.py', '/root/pkg/mod.py'})
+        
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
     
