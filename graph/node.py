fe8582674b83b71cccb3ce38ca0a12b1f326aa90
Iain Peet <iain@ipeet.org>
2017-12-17 15:17:24 -0800

    Backfill graph.node typechecking + take out trash

--- graph/node.py
+++ graph/node.py
@@ -8,45 +8,17 @@
 
 import collections
 from graph.edge import Edge
+from typing import Set
+from workspace.path import Path
 
 class Node(object):
-    def __init__(self):
-        self.outgoing = set() # {Edge}
-        self.incoming = set() # {Edge}
+    def __init__(self) -> None:
+        self.outgoing: Set[Edge] = set() 
+        self.incoming: Set[Edge] = set()
 
 class File(Node):
-    def __init__(self, path):
+    def __init__(self, path:Path) -> None:
         super(File, self).__init__()
         self.path = path
 
 Location = collections.namedtuple('Location', ['file', 'line', 'column'])
-        
-class Name(Node):
-    '''
-    Something which has a name and is declared in a a file.
-    (variable, function, class, etc.)
-    '''
-    def __init__(self, name):
-        super(Name, self).__init__()
-        self.name = name
-        # List of Location this name is declared (multiple being expected,
-        # considering current lack of any namespace comprehension)
-        self.declarations = []
-        
-class Function(Name):
-    pass
-    
-class Variable(Name):
-    pass
-    
-class Class(Name):
-    pass
-    
-class Call(Name):
-    pass
-    
-class Reference(Name):
-    '''
-    An assignment / load of some variable.
-    '''
-    pass
